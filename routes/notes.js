c2cb8413cb3bbadfdf4d593d3830f2729386ff28
rishurana8 <rishurana256@gmail.com>
2024-05-01 01:22:30 +0530

    deleting api added

--- routes/notes.js
+++ routes/notes.js
@@ -44,4 +44,47 @@ router.get('/addnote',fetchuser,[
 
 })
 
+// update note or update wahi note krna hai jiska yeh note ho
+router.put('/updatenote/:id',fetchuser,async(req,res)=>{
+    const {title,description,tag} = req.body;
+    const newNote = {};
+    if(title) {newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    let note = await Note.findById(req.params.id);
+    if(!note){
+        return res.status(404).send("Not Found");
+    }
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id,{$set: newNote},{new:true});
+    res.json({note});
+})
+
+
+
+// api for deleting a note
+router.put('/deletnote/:id',fetchuser,async(req,res)=>{
+    const {title,description,tag} = req.body;
+    // ab hume yaha pe bs yeh verify krna hai ki jo insaan iss note ko delet kr rha hai woh usi ka hai na 
+
+    let note = await Note.findById(req.params.id);
+    // if note not present toh delte kr hi nahi skte 
+    if(!note){
+        return res.status(404).send("Not Found");
+    }
+//    if user ne jo id beji hai woh match ni kr rhi so allow ni krenge delete krne ko 
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({"Success":"Note has been deleted"});
+})
+
+
+
 module.exports = router;
